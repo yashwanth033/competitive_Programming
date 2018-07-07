114798baae39b5f58c2caec566628f29f1050a28
yashwanth033 <kadiyamyaswant@gmail.com>
2018-07-07 12:56:27 +0530

    week2 day6 7-07-18

---
+++ competitive programming/Week2/Day6/InShuffle.py
@@ -0,0 +1,16 @@
+import random
+
+
+def shuffle(arr):
+    n = len(arr)
+    for i in range(n - 1, 0, -1):
+        j = random.randint(0, i)
+        arr[i], arr[j] = arr[j], arr[i]
+
+
+sample_list = [1, 2, 3, 4, 5]
+print('Sample list:', sample_list)
+
+print('Shuffling sample list...')
+shuffle(sample_list)
+print(sample_list)
